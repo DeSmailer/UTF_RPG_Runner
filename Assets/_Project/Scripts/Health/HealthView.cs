32f14603ef5180cd5c79b179acc7030bcdcc4ebc
Pyliavskyi Dmytro <dmytro.pyliavskyi@nure.ua>
2024-05-05 16:24:36 +0300

    added HealthView

--- Assets/_Project/Scripts/Health/HealthView.cs
+++ Assets/_Project/Scripts/Health/HealthView.cs
@@ -3,16 +3,20 @@ using UnityEngine.UI;
 
 public class HealthView : MonoBehaviour
 {
-  [SerializeField] private Health health ;
+  [SerializeField] private Health health;
   [SerializeField] private Slider hpSlider;
 
   private void Start()
   {
     health.OnChange.AddListener(Display);
+    Display();
   }
 
   public void Display()
   {
+    Debug.Log(health.MaxHp);
+    Debug.Log(health.CurrentHp);
+    Debug.Log(health.GetHpPercentage());
     hpSlider.value = health.GetHpPercentage();
   }
 }
